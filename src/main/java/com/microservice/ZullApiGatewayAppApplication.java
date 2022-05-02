c41c11e2f7c9475f96b63fd8664633ac42188108
Aniket <kumaraniket900@gmail.com>
2022-05-02 21:42:19 +0530

    Initial Aniket Commit

---
+++ src/main/java/com/microservice/ZullApiGatewayAppApplication.java
@@ -0,0 +1,18 @@
+package com.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZullApiGatewayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZullApiGatewayAppApplication.class, args);
+		System.out.println("ZullApiGatewayAppApplication Has Started !!");
+	}
+
+}
